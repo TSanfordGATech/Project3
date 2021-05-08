8d3999a103106eb86d3ea204c395196517545b6f
Obedia Lawrence <omlawrence24@gmail.com>
2021-05-08 13:06:04 -0400

    updated file stucture

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css";
 // test
 
 ReactDOM.render(
