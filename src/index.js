13df5e630ceafcbf5761f2857bb7eb4a0b7c9da5
Heather Smith <halexsmith86@gmail.com>
2021-05-08 11:19:48 -0400

    Added models directory and user model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// test
 
 ReactDOM.render(
   <React.StrictMode>
