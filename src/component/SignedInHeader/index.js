792bcd42bd98ce812eab5d2f7c7037dbf086c2a9
Obedia Lawrence <omlawrence24@gmail.com>
2021-05-15 12:01:56 -0400

    router changes

--- src/component/SignedInHeader/index.js
+++ src/component/SignedInHeader/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// (document).foundation();
+
 
 function SignedInHeader() {
 
@@ -9,27 +9,10 @@ function SignedInHeader() {
         <div className="header">
           <div className="jumbotron">
             <h1>Brainstormers</h1>
+            <button data-target="modallogout" class="btn modal-trigger">Modal</button>
+         
 
-          <p><button class="button log-button" data-open="sign-in">Log Out</button></p>
-
-            <div class="reveal" id="sign-in" data-reveal>
-              
-              <h1>Awesome!</h1>
-              <p class="lead">I have another modal inside of me!</p>
-              <button class="button" data-open="log-in">Click me for another modal!</button>
-              <button class="close-button" data-close aria-label="Close reveal" type="button">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-
-
-            <div class="reveal" id="log-in" data-reveal>
-              <h2>ANOTHER MODAL!!!</h2>
-              <button class="close-button" data-close aria-label="Close reveal" type="button">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-
+           
           </div>
 
         </div>
