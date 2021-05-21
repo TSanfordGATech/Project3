2fef713c1c810487940b4584f1d02caba6ebaeef
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-05-21 14:37:11 -0400

    updating my story board

--- src/component/game3/src/scenes/Welcome/index.js
+++ src/component/game3/src/scenes/Welcome/index.js
@@ -14,10 +14,20 @@ function Welcome(props) {
             <div className="container" id="cont3">
                 <Row>
                     <Col size="lg-8">
-                        <p className="intro3"> The year is 2020 and it is almost summer time.... they swore the virus would go away and life would resume normal. You planned a get away vacation with the family...</p>
+                        <p className="intro3"> The year is 2020...... the world and society has taken a turn for the worst. Since vacation options are limited, and rather risky.... you and your family decide to think outside the box and explore other options to get away. Since COVID has struck, you all now work from home and decide to purchase a van and travel the world together. All was going well until the gas crisis.... that was not a real crisis.... hit and you broke down on the side of the road with an empty tank...... </p>
                         <br></br>
                         <br></br>
-                        <p className="until3">but things didn't go as planned....</p>
+                        <p className="until3">and AAA had no gas to come and get you either. </p>
+                        <br></br>
+                        <br></br>
+                        <p className="intro3"> Night time came and you all decided to take a doze in the van while you waited to be rescued. While everyone was snoring away - you heard something unusual outside so you ventured out to see what it was.</p>
+                        <br></br>
+                        <br></br>
+                        <p className="until3">WHAM</p>
+                        <br></br>
+                        <br></br>
+                        <p className="intro3"> Everything has gone dark..... are you even concious?</p>
+                        <br></br>
                     </Col>
                 </Row>
             </div>
