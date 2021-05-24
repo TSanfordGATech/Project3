b137f8901af7f252d6aee8cdfdcae06c76bc4139
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-05-23 21:00:41 -0400

    adding in all of my base files for the remaining objects in the scenes and updating their routes

---
+++ client/src/component/game3/src/scenes/Background/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Background() {
+    return (
+       <h1>Background</h1>
+    )
+}
+
+export default Background
---
+++ client/src/component/game3/src/scenes/Bookshelf/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bookshelf() {
+    return (
+       <h1>Bookshelf</h1>
+    )
+}
+
+export default Bookshelf
