69fc6cfb56d8fc013c596f774504d6b6b0451cb5
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-05-26 16:39:08 -0400

    button direction for wine barrel completed

--- client/src/component/game3/src/scenes/Office/index.js
+++ client/src/component/game3/src/scenes/Office/index.js
@@ -3,21 +3,24 @@ import { Link, Route } from "react-router-dom";
 import "./style.css";
 
 function Office(props) {
- 
-return (
-  <div className= "parent">
-    <div className="dung"></div>
-    {/* this will be the barrel they can click. Determine what you want to put into it if anything. */}
-    <div className="clickbarrel"></div>
-    {/* door they can click */}
-    <div className="dungdoor"></div>
-    {/* crate they can click */}
-    <div className="crate"></div>
-    {/* winebarrel they can click */}
-    <div className="wbarrel"></div>
-  </div>
-          
-);
+
+  return (
+    <div className="parent">
+      <div className="dung"></div>
+      {/* this will be the barrel they can click. Determine what you want to put into it if anything. */}
+      <div className="clickbarrel"></div>
+      {/* door they can click */}
+      <div className="dungdoor"></div>
+      {/* crate they can click */}
+      <div className="crate"></div>
+      {/* winebarrel they can click */}
+      <div className="wbarrel2">
+        <Link to="/Wbarrel">
+            <button className="wbarrel"></button>
+        </Link></div>
+    </div>
+
+  );
 }
 
 export default Office;
