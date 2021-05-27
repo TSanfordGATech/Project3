99ae03c5e69b3e2989edc6ce512539a556c20ea2
Tori Sanford <tori.danielle.clark91@gmail.com>
2021-05-26 22:08:36 -0400

    finished game

--- client/src/component/game3/src/scenes/Run/index.js
+++ client/src/component/game3/src/scenes/Run/index.js
@@ -3,12 +3,17 @@ import { Link, Route } from "react-router-dom";
 import "./style.css";
 
 function Run() {
-    return (
-        <div className="dung">
-          <p>Run page</p>
-        </div>
-
-    );
+  return (
+    <div className="dung">
+      <div className="infocard66">
+          <Link to="/">
+            <div className="modal-footer">
+              <button className="modal-close waves-effect btn-small" >RUN!</button>
+            </div>
+          </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Run;
